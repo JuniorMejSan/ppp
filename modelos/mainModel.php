5f659b5de7af80fc7aa0f033fe7c571d8e9ca955
Jumimesa <juniormejia954@gmail.com>
2024-05-22 01:27:13 -0500

    Anvances semana 3

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -19,4 +19,63 @@
             $sql -> execute();
             return $sql;
         }
+
+        //fucnion para encriptar cadenas o textos planos (hash)
+        public function encryption($string){
+			$output=FALSE;
+			$key=hash('sha256', secret_key);
+			$iv=substr(hash('sha256', secret_iv), 0, 16);
+			$output=openssl_encrypt($string, method, $key, 0, $iv);
+			$output=base64_encode($output);
+			return $output;
+		}
+
+        //funcion para desenciptar cadenas (hash)
+		protected static function decryption($string){
+			$key=hash('sha256', secret_key);
+			$iv=substr(hash('sha256', secret_iv), 0, 16);
+			$output=openssl_decrypt(base64_decode($string), method, $key, 0, $iv);
+			return $output;
+		}
+
+        //funcion para generear codigos aleatorios para identificar las ventas
+        protected static function generar_codigo_aleatorio($letra, $longitud, $numero){ //ejemplo P123-1 $letra es para P, $longitud para la cantidad de digitos y $numero es el correlativo segun la base de datos
+            for ($i=1; $i <= $longitud ; $i++) { //hace iteraciones para elejir numero al azar, segun la longitud
+                $aleatorio = rand(0,9); //rand escoge un numero al azar entre el 0 y el 9
+                $letra .= $aleatorio; //concatena la letra con el numero aleatorio como el ejemplo P123-1
+            }
+            return $letra."-".$numero; //concatena todos las variables para tener el codigo aleatorio
+        }
+
+        //funcion para evitar inyecciones sql, va a limpiar textos o caracteres cuando se envien datos mediante formularios
+        protected static function limpiar_cadena($cadena){
+            $cadena = trim($cadena); //devuelve la cadena de texto sin de los espacios en blanco en ambos extremos
+            $cadena = stripslashes($cadena); //quita las barras de un string 
+            $cadena = str_ireplace("<script>","", $cadena); //busca si el string contiene "<script>" y lo reemplaza por vacio
+            $cadena = str_ireplace("</script>","", $cadena);
+            $cadena = str_ireplace("<script src","", $cadena);
+            $cadena = str_ireplace("<script type=","", $cadena);
+            $cadena = str_ireplace("SELECT * FROM","", $cadena);
+            $cadena = str_ireplace("DELETE * FROM","", $cadena);
+            $cadena = str_ireplace("INSERT INTO","", $cadena);
+            $cadena = str_ireplace("DROP TABLE","", $cadena);
+            $cadena = str_ireplace("DROP DATABASE","", $cadena);
+            $cadena = str_ireplace("TRUNCATE TABLE","", $cadena);
+            $cadena = str_ireplace("SHOW TABLES","", $cadena);
+            $cadena = str_ireplace("SHOW DATABASES","", $cadena);
+            $cadena = str_ireplace("<?php","", $cadena);
+            $cadena = str_ireplace("?>","", $cadena);
+            $cadena = str_ireplace("--","", $cadena);
+            $cadena = str_ireplace("<","", $cadena);
+            $cadena = str_ireplace(">","", $cadena);
+            $cadena = str_ireplace("[","", $cadena);
+            $cadena = str_ireplace("]","", $cadena);
+            $cadena = str_ireplace("^","", $cadena);
+            $cadena = str_ireplace("==","", $cadena);
+            $cadena = str_ireplace(";","", $cadena);
+            $cadena = str_ireplace("::","", $cadena);
+            $cadena = stripslashes($cadena);
+            $cadena = trim($cadena);
+            return $cadena;
+        }
     }
